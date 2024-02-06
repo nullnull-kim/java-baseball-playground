9b030e42200fd3dda6bebd158bee103102456c13
nullnull.kim <nullnull.kim@gmail.com>
2024-02-06 21:52:54 +0900

    요구사항 3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -38,5 +38,20 @@ public class StringTest {
         String input = "(1,2)";
         String substring = input.substring(1, 4);
         assertThat(substring).isEqualTo("1,2");
+    }
+
+    /**
+     * 요구사항 3
+     * "abc" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.
+     * String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다.
+     * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
+     */
+    @DisplayName("")
+    @Test
+    void charAt() {
+        String input = "abc";
+        assertThatThrownBy(() -> {
+            input.charAt(4);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
