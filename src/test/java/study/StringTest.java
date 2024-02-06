6e4260c6e80d437bc7381ecd8a2f984eba2fff71
nullnull.kim <nullnull.kim@gmail.com>
2024-02-06 21:21:25 +0900

    요구사항 1 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,29 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /**
+     * 요구사항 1
+     * "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+     * "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+     * 힌트
+     * 배열로 반환하는 값의 경우 assertj의 contains()를 활용해 반환 값이 맞는지 검증한다.
+     * 배열로 반환하는 값의 경우 assertj의 containsExactly()를 활용해 반환 값이 맞는지 검증한다.
+     */
+    @Test
+    void split() {
+        String input = "1,2";
+        String[] splitActual = input.split(",");
+        assertThat(splitActual).contains("1", "2");
+
+        assertThat(splitActual[0].split(",")).containsExactly("1");
     }
 }
