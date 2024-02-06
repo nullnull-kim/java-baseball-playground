097f19ca637b6e94cbcfedc33dac0f9c3993c56a
nullnull.kim <nullnull.kim@gmail.com>
2024-02-06 22:10:32 +0900

    요구사항 2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,9 +3,14 @@ package study;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -28,4 +33,24 @@ public class SetTest {
     void size() {
         Assertions.assertThat(numbers.size()).isEqualTo(3);
     }
+
+    /**
+     * 요구사항 2
+     * Set의 contains() 메소드를 활용해 1, 2, 3의 값이 존재하는지를 확인하는 학습테스트를 구현하려한다.
+     * 구현하고 보니 다음과 같이 중복 코드가 계속해서 발생한다.
+     * JUnit의 ParameterizedTest를 활용해 중복 코드를 제거해 본다.
+     */
+//    @Test
+//    void contains() {
+//        assertThat(numbers.contains(1)).isTrue();
+//        assertThat(numbers.contains(2)).isTrue();
+//        assertThat(numbers.contains(3)).isTrue();
+//    }
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3}) // six numbers
+    void contains(int number) {
+        assertTrue(numbers.contains(number));
+    }
+
+
 }
