04c85dbd80686c93f7a5f2204cac29ce00a109be
nullnull.kim <nullnull.kim@gmail.com>
2024-02-06 23:03:20 +0900

    요구사항 3 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
@@ -52,5 +54,22 @@ public class SetTest {
         assertTrue(numbers.contains(number));
     }
 
-
+    /**
+     * 요구사항 3
+     * 요구사항 2는 contains 메소드 결과 값이 true인 경우만 테스트 가능하다. 입력 값에 따라 결과 값이 다른 경우에 대한 테스트도 가능하도록 구현한다.
+     * 예를 들어 1, 2, 3 값은 contains 메소드 실행결과 true, 4, 5 값을 넣으면 false 가 반환되는 테스트를 하나의 Test Case로 구현한다.
+     * 힌트
+     * Guide to JUnit 5 Parameterized Tests 문서에서 @CsvSource를 활용한다.
+     */
+//    @ParameterizedTest
+//    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+//    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+//        String actualValue = input.toLowerCase();
+//        assertEquals(expected, actualValue);
+//    }
+    @ParameterizedTest
+    @CsvSource(value={"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains(int number, boolean expected) {
+        assertEquals(numbers.contains(number), expected);
+    }
 }
